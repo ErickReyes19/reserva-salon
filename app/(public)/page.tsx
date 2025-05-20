8d8f3b70210989ff507d42d150e63ab7a8f807dc
ereyes0212 <erick.reyes@tiempo.hn>
2025-05-20 14:22:29 -0600

    cambio en el page

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -3,11 +3,9 @@ import HeroSection from "./components/sections/hero-section";
 import AboutSection from "./components/sections/about-section";
 import PhotographersSection from "./components/sections/photographers-section";
 import ServicesSection from "./components/sections/services-section";
-import PortfolioSection from "./components/sections/portafolio-section";
 import TestimonialsSection from "./components/sections/testimonials";
 import BookingSection from "./components/sections/booking-section";
 import Footer from "./components/footer";
-import HeaderClient from "./components/header-client";
 import Header from "./components/header";
 
 export default function PublicPage() {
@@ -19,7 +17,6 @@ export default function PublicPage() {
         <AboutSection />
         <PhotographersSection />
         <ServicesSection />
-        <PortfolioSection />
         <TestimonialsSection />
         <BookingSection />
         <Footer />
