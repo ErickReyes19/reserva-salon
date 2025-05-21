db13afd5df766b23e42660e0051085b962b5b142
ereyes0212 <erick.reyes@tiempo.hn>
2025-05-21 15:54:00 -0600

    cambios

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,17 @@
+
 // next.config.js
+import { PrismaPlugin } from '@prisma/nextjs-monorepo-workaround-plugin';
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     domains: ['res.cloudinary.com'],
+  },
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      config.plugins.push(new PrismaPlugin());
+    }
+    return config;
   },
 };
 
